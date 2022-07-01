90a94d434debf50ee48d08a38fb4a3058c77c325
Kayo Céshar Rêgo Dias dos Santos <kayoceshar@hotmail.com>
2022-07-01 18:03:25 -0300

    styled components

---
+++ modulo3/styled-components/ex-styled-components/src/style.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const CabecalhoHeader=styled.header`
+display: flex;
+align-items: center;
+justify-content: center;
+margin-left: 1px;
+`
+export const CabecalhoImagem=styled.img`
+width: 40px;
+height: 40px;
+`
+export const CabecalhoTitulo=styled.h1`
+width: 15px;
+`
+
+export const MainContainer=styled.div`
+
+display: flex;
+height: 470px;
+
+
+`
+
+export const MainBarraEsquerda=styled.aside`
+background-color: #ffa500;
+width: 12%;
+
+
+
+    
+`
+
+export const MainBarraCentral=styled.main`
+width: 76%;
+form{
+    position: fixed;
+    bottom: 25px;
+    display: flex;
+    gap: 5px;
+    margin-left: 15px;
+}
+
+`
+
+export const MainBarraDireita=styled.aside`
+background-color: #ffa500;
+width: 12%;
+
+
+`
+
+export const RodapeFooter=styled.footer`
+background-color: gray;
+
+width: 100%;
+padding: 0;
+height: 20px;
+position: fixed;
+bottom: 0;
+
+
+p{
+    text-align: center;
+   margin-top: 0;
+}
+
+`
