20522df29719739c47852f29ca58b9208da23e2d
Kayo Céshar Rêgo Dias dos Santos <kayoceshar@hotmail.com>
2022-07-08 19:11:20 -0300

    whatslab

---
+++ modulo3/projeto-whatslab/src/App.js
@@ -0,0 +1,15 @@
+import Cabecalho from './Components/Cabecalho';
+import Main from './Components/Main';
+import Rodape from './Components/Rodape';
+
+function App() {
+  return (
+    <div className="App">
+     <Cabecalho/>
+     <Main/>
+     <Rodape/>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo3/projeto-whatslab/src/Components/Rodape.js
@@ -0,0 +1,10 @@
+import React from "react";
+import{RodapeFooter} from "../style"
+
+export default function Rodape() {
+    return(
+        <RodapeFooter>
+            <p>Copyright © Kayo Céshar</p>
+        </RodapeFooter>
+    )
+}
