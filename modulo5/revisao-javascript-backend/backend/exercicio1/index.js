2f8d56a2868ba2e4d4e7882599768be1f7553667
Kayo Céshar Rêgo dias dos Santos <78767807+kayoceshar@users.noreply.github.com>
2022-10-07 18:14:38 -0300

    exercicio 1 finalizado

--- modulo5/revisao-javascript-backend/backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio1/index.js
@@ -1,1 +1,17 @@
-console.log("exercicio 4");
+const converteCelsius = (temperaturaCelsius, grandeza) => {
+    if(typeof temperaturaCelsius !== 'number'){
+        return `Erro. Parâmetro inválido ${temperaturaCelsius}.`
+    }else if (grandeza === "K") {
+        return `${temperaturaCelsius}° Celsius é equivalente a ${temperaturaCelsius + 273} Kelvin` 
+    } else if (grandeza === "F") {
+        return `${temperaturaCelsius}° Celsius é equivalente a ${temperaturaCelsius*1.8 + 32} Farenheit`
+    }  else {
+        return `Erro. Parâmetro inválido ${grandeza}.`
+    }
+}
+
+
+console.log(converteCelsius(30,"K"));
+console.log(converteCelsius(30,"F"));
+console.log(converteCelsius(30,"G"));
+console.log(converteCelsius("trinta","k"));
