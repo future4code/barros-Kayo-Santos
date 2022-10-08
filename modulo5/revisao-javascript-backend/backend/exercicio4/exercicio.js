9037934e9cb4f210ed1ecddb440a8d00317f3d61
Kayo Céshar Rêgo dias dos Santos <78767807+kayoceshar@users.noreply.github.com>
2022-10-08 19:35:53 -0300

    exercicio 4

--- modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -1,1 +1,33 @@
-console.log("exercicio 4");
+const primeiraLista = [
+    {
+      nome: "Banana"
+    },
+    {
+      nome: "Laranja"
+    }
+  ];
+  
+  const segundaLista = [
+    {
+      nome: "Laranja"
+    },
+    {
+      nome: "Cebola"
+    }
+  ];
+  
+  const geraItensUnicos = (primeira, segunda) => {
+    const listaunica = [...primeira, ...segunda];
+  
+    for (let i = 0; i < listaunica.length; i++) {
+      for (let j = i + 1; j < listaunica.length; j++) {
+        if (listaunica[i].nome === listaunica[j].nome) {
+          listaunica.splice(j, 1);
+        }
+      }
+    }
+  
+    return listaunica;
+  };
+  
+  console.log(geraItensUnicos(primeiraLista, segundaLista));
