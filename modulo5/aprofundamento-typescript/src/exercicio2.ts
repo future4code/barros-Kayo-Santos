bd862c254b4ec37068e9beeb6d45a38c3bba4c0f
Kayo Céshar Rêgo dias dos Santos <78767807+kayoceshar@users.noreply.github.com>
2022-10-18 17:55:44 -0300

    Aula - Aprofundamento em Typescript

---
+++ modulo5/aprofundamento-typescript/src/exercicio2.ts
@@ -0,0 +1,31 @@
+//a) entrada: array de números, saída: ordena os números  e faz a média deles.
+
+function obterEstatisticas(numeros:number[]):any {
+
+    const numerosOrdenados = numeros.sort(
+        (a:number, b:number) => a - b
+    )
+
+    let soma:number = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+console.log(obterEstatisticas([6,3]));
+
+
+//b) number e o retorno é type any
+
+//c)
+type amostraDeDados = {
+    numeros: number[],
+    obterEstatisticas: (numeros:number[]) => {estatísticas:string}}
