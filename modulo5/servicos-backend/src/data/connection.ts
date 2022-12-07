cc8785d22489b82baa37d7632b57933175ea1181
Kayo Céshar Rêgo dias dos Santos <78767807+kayoceshar@users.noreply.github.com>
2022-12-06 21:17:40 -0300

    inicio

--- modulo5/servicos-backend/src/data/connection.ts
+++ modulo5/servicos-backend/src/data/connection.ts
@@ -3,14 +3,3 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const connection = knex({
-    client: 'mysql',
-    connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_SCHEMA,
-        port: 3306,
-        multipleStatements: true
-    }
-})
