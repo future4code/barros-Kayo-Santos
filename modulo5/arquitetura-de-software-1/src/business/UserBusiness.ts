2c28532bf564b5dd063286ff275069e246bf1a12
Kayo Céshar Rêgo dias dos Santos <78767807+kayoceshar@users.noreply.github.com>
2023-01-17 21:08:47 -0300

    arquitetura

---
+++ modulo5/arquitetura-de-software-1/src/business/UserBusiness.ts
@@ -0,0 +1,36 @@
+import { UserDataBase } from "../data/UserDataBase";
+
+export class UserBusiness {
+    
+
+createUser = async (user:any): Promise<void> => {
+
+        try{
+
+            const {email, name, password} = user
+
+            if(!name || !email || !password ){
+                throw new Error("Please fill all the fields");
+            }
+
+            if(email.indexOf("@") === -1){
+                throw new Error("Invalid Email");
+            }
+
+            if(password.length < 6){
+                throw new Error("Password must have at least 6 characters");
+            }
+
+            const id = Date.now().toString();
+
+            const userDataBase = new UserDataBase()
+          
+            await userDataBase.createUser({id,email,name, password});             
+         
+            
+        }catch(error:any){
+            throw new Error( error.message || "Error creating user. Please check your system administrator.");
+        }
+    }
+    
+}
